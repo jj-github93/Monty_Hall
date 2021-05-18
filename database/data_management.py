9e37227c42f4d5283d823b6a8670096438bff165
jj-github93 <joontravelbug+github@gmail.com>
2021-05-18 13:34:27 +0800

    Database handling added
    SQLAlchemy database handling has been added.
    New python file which does that handling added data_management.py)
    requirements.txt file added

--- database/data_management.py
+++ database/data_management.py
@@ -5,6 +5,8 @@ from sqlalchemy.exc import IntegrityError
 
 Base = declarative_base()
 
+database = "sqlite:///users.db"
+
 
 class User(Base):
     __tablename__ = "Users"
@@ -15,31 +17,71 @@ class User(Base):
     losses = Column("Losses", Integer, default=0)
     win_loss = Column("Win/Loss", Float, default=0.0)
 
+    @staticmethod
+    def session_maker(db):
+        """
+            Function to create a new session.
+        """
 
-engine = create_engine("sqlite:///users.db", echo=True)
+        engine = create_engine(db)
 
-Base.metadata.create_all(bind=engine)
+        Base.metadata.create_all(bind=engine)
 
-Session = sessionmaker(bind=engine)
+        Session = sessionmaker(bind=engine)
 
-session = Session()
+        session = Session()
 
+        return session
 
-def add_user():
-    new_user = User()
+    @staticmethod
+    def add_user(db):
+        """
+            Function that gets a unique username from the user.
+            Adds the new user to the db.
+        """
+        session = User.session_maker(db)
 
-    try:
-        new_user.username = input("Enter a unique username: \n")
-        session.add(new_user)
+        new_user = User()
+        while True:
+            try:
+                new_username = input("Enter a unique username: \n").upper()
+                new_user.username = new_username
+                session.add(new_user)
+                session.commit()
+            except IntegrityError:
+                print("Username entered is already taken \n")
+                session.rollback()
+                session.close()
+            else:
+                session.close()
+
+        return new_username
+
+    @staticmethod
+    def update_user(user_name, games, wins, losses, win_loss, db):
+        """
+            Function that searches for a user in the table by their username.
+            Once found it updates their statistics with the parse in values.
+        """
+
+        # Creates a new session
+        session = User.session_maker(db)
+
+        # Searches table by unique username
+        user = session.query(User).filter(User.username == user_name).with_for_update().one()
+
+        # Updates the selected users details
+        user.wins = wins
+        user.games = games
+        user.losses = losses
+        user.win_loss = win_loss
+
+        # commits session
         session.commit()
-    except IntegrityError:
-        print("Username entered is already taken \n")
-        session.rollback()
-
-    finally:
         session.close()
 
-
-add_user()
-
-session.close()
+    @staticmethod
+    def init_db(db):
+        session = User.session_maker(db)
+        session.commit()
+        session.close()
